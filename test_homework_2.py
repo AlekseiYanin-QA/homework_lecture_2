6052321a396637fb26e75cd1040dd08742ba066f
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-01-12 09:14:06 +0300

    Дописал should have.exact_text()

--- test_homework_2.py
+++ test_homework_2.py
@@ -8,4 +8,4 @@ def test_google_available_text(browser_scale):
 def test_google_not_available_text(browser_scale):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('Какой$то бред товориться#$@$$%$%$%#%^%^&%@&%^@$').press_enter()
-    browser.element('.card-section [role="heading"]').should(have.text('По запросу Какой$то бред товориться#$@$$%$%$%#%^%^&%@&%^@$ ничего не найдено. '))
+    browser.element('.card-section [role="heading"]').should(have.exact_text('По запросу Какой$то бред товориться#$@$$%$%$%#%^%^&%@&%^@$ ничего не найдено. '))
