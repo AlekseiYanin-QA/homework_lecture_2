d1d492b725cb52acf72f00f2d5c5b907c4baa465
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-01-12 07:17:46 +0300

    Написал код из перевого урока

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture()
+def browser_size():
+    browser.config.window_width = 800
+    browser.config.window_height = 600
--- test_homework_2.py
+++ test_homework_2.py
@@ -1,7 +1,14 @@
 from selene import browser, be, have
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('yasha/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+# browser.open('https://google.com')
+# browser.element('[name="q"]').should(be.blank).type('yasha/selene').press_enter()
+# browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
+
+
+def test_ya(browser_size):
+    browser.open('https://ya.ru/')
+    browser.element('[id="text"]').should(be.blank).type('yasha/selene').press_enter()
+    browser.element('[class="main serp i-bem"] [type="button"]').should(be.blank).click()
+    browser.element('[id="search-result"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
