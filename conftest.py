c1dfb4aef53cfe5e99bf5af738b007cedacf97ed
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-01-12 19:53:56 +0300

    Поправил код, согласно рекомендациям

--- conftest.py
+++ conftest.py
@@ -1,13 +1,9 @@
 import pytest
 from selene import browser
 
-@pytest.fixture()
-def browser_open():
-    browser.open('https://google.com')
-    yield
-    browser.quit()
-
-@pytest.fixture()
+@pytest.fixture(scope='function', autouse=True)
 def browser_scale():
     browser.config.window_width = 1200
     browser.config.window_height = 800
+    yield
+    browser.quit()
--- test_homework_2.py
+++ test_homework_2.py
@@ -1,10 +1,12 @@
 from selene import browser, be, have
 
-def test_google_available_text(browser_open, browser_scale):
+def test_google_available_text():
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('#search').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_google_not_available_text(browser_open, browser_scale):
+def test_google_not_available_text():
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('1uвыпывпвыпы5!@#$%thtrtr^trhrhrм').press_enter()
     browser.element('[id="result-stats"]')\
         .should(have.text('Результатов: примерно 0'))
